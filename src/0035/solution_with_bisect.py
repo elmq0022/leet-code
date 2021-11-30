6e6cd6b13da7e4b15f158ad65c6078dea76a095a
LeakedMemory <elmq0022@umn.edu>
2021-11-29 22:57:28 -0600

    adds more solutions

---
+++ src/0035/solution_with_bisect.py
@@ -0,0 +1,11 @@
+"""
+Knowing the standard library pays off.
+"""
+
+from bisect import bisect_left
+from typing import List
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        return bisect_left(nums, target)
