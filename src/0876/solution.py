17cb7821892c3a703b6add48301da714c064ef23
LeakedMemory <elmq0022@umn.edu>
2021-12-02 21:35:32 -0600

    adds solution for middle of linked list

---
+++ src/0876/solution.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        p1 = p2 = head
+
+        while p2.next and p2.next.next:
+            p1 = p1.next
+            p2 = p2.next.next
+
+        if p2.next:
+            p1 = p1.next
+
+        return p1
