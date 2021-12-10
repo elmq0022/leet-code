a70ca6f3a4ec80641af77036dbc34d976c742fba
LeakedMemory <elmq0022@umn.edu>
2021-12-10 11:22:19 -0600

    adds house robber solution

---
+++ src/0198/solution.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        result = [0] * (n + 2)
+        result[0] = 0
+        result[1] = nums[0]
+
+        for i in range(2, n + 1):
+            result[i] = max(result[i - 2] + nums[i - 1], result[i - 1])
+
+        return result[n]
