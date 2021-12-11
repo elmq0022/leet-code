6931d7ab2ee21fadaabc433c2712471873ed7da8
LeakedMemory <elmq0022@umn.edu>
2021-12-10 19:27:06 -0600

    adds  improved solution

--- src/0198/solution.py
+++ src/0198/solution.py
@@ -12,3 +12,14 @@ class Solution:
             result[i] = max(result[i - 2] + nums[i - 1], result[i - 1])
 
         return result[n]
+
+
+class Solution2:
+    def rob(self, nums: List[int]) -> int:
+        h1, h2 = 0, 0
+
+        for val in nums:
+            # best decsion at "h3"
+            h2, h1 = max(h1 + val, h2), h2
+
+        return h2
